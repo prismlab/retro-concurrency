67dc61a6db583b07a8929e6941d9578e74170d70
KC Sivaramakrishnan <kc@kcsrk.info>
2020-10-31 13:02:09 +0530

    update static semantics

--- code/handler.ml
+++ code/handler.ml
@@ -3,11 +3,44 @@ module type Handler = sig
   type ('a,'b) continuation
   type ('a, 'b) handler =
     { ret_case : 'a -> 'b;
-      eff_case_1 : 'c. 'c eff -> ('c, 'b)continuation -> 'b;
-      eff_case_2 : 'd. 'd eff -> ('d, 'b)continuation -> 'b; }
+      eff_case : 'c. 'c eff -> ('c, 'b)continuation -> 'b }
 
   val perform : 'a eff -> 'a
   val continue : ('a,'b) continuation -> 'a -> 'b
   val discontinue : ('a,'b) continuation -> exn -> 'b
   val match_ : (unit -> 'a) -> ('a, 'b) handler -> 'b
+end
+
+module Handler : Handler = struct
+  type ('a, 'b) handler =
+    { ret_case : 'a -> 'b;
+      eff_case : 'c. 'c eff -> ('c, 'b)continuation -> 'b }
+
+  let perform e = failwith "not implemented"
+  let continuat k v = failwith "not implemented"
+  let discontinue k e = failwith "not implemented"
+
+  effect E : int -> int
+  effect F : string -> string
+
+  type _ eff += E : int -> int eff
+              | F : string -> string eff
+
+  type t = A | B
+
+  let run f = match f () with
+  | A -> true
+  | B -> false
+  | effect (E 42) k -> continue k 43; true
+  | effect (F "42") k -> continue k "43"; false
+
+  let eff_case : type c. c eff -> (c, 'b) continuation -> 'b =
+    function | E 42 -> (fun k1 -> continue k1 43)
+             | F "42" -> (fun k2 -> continue k2 "43")
+
+  let h1 =
+    { ret_case = (function A -> true
+                    | B -> false);
+      eff_case; }
+
 end
